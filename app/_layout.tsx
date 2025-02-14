4a9bdf316bec584e9ad6ea362cc71bf4cf22f495
amal <amalvincent24@gmail.com>
2025-02-15 00:48:28 +0530

    removing color scheme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,9 @@
 import { Stack } from 'expo-router'
-import { useColorScheme } from 'react-native'
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider
-} from '@react-navigation/native'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   )
 }
