43ada88042d486e645eeefaf9dfc0d38a3b09bb0
amal <amalvincent24@gmail.com>
2025-02-16 10:19:18 +0530

    gitignore

--- app.config.js
+++ app.config.js
@@ -7,11 +7,13 @@ export default {
         projectId: '5e58067a-7edb-4fbd-81d8-dd3cecb01736'
       }
     },
+    icon: './assets/images/icon.png',
     android: {
-      package: 'com.flicks.app'
+      package: 'com.flicks.app',
     },
     ios: {
-      bundleIdentifier: 'com.flicks.app'
+      bundleIdentifier: 'com.flicks.app',
+      icon: './assets/images/icon.png'
     }
   }
 }
